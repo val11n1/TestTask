4e022b24f44fd1afc3500398032e368a2814da55
val11n1 <76488718+val11n1@users.noreply.github.com>
2022-05-26 14:14:32 +0300

    Initial Commit

---
+++ PryanikiTestTask/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  PryanikiTestTask
+//
+//  Created by Valeriy Trusov on 26.05.2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
